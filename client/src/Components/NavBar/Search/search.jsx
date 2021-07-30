1703f6635120f1ce46749b1464a2e09152a631f1
unknown <aguerofranco2002@gmail.com>
2021-07-30 10:24:26 -0300

    refactor(client/navBar/search): change of place and add style

--- client/src/Components/NavBar/Search/search.jsx
+++ client/src/Components/NavBar/Search/search.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getPokeByName, setearStore } from '../../../Actions/actions'; 
 import Lupa from './lupa.png'
 import s from './search.module.css'
@@ -21,10 +22,10 @@ import s from './search.module.css'
         setInput({name:''})
     }
      return (
-         
+        
         <div className={s.container}>
             <input type="text" className={s.input} name="name" value={input.name} onChange={(e) => handleChange(e)} placeholder="search by name..."  />
-            <button onClick={(e) => handleSubmit(e)} id={s.btnSubmit}><img src={Lupa} alt="lupa search" id={s.IMG} /></button> 
+            <button onClick={(e) => handleSubmit(e)} id={s.btnSubmit}><Link to='/home'><img src={Lupa} alt="lupa search" id={s.IMG} /></Link></button> 
         </div>
          
      )
