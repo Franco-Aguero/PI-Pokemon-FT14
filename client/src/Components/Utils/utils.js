12539d73df01cd4fc0b032fb2ff43642f6dfeee0
unknown <aguerofranco2002@gmail.com>
2021-07-30 10:12:31 -0300

    refactor(client/utils): add folder style

---
+++ client/src/Components/Utils/utils.js
@@ -0,0 +1,89 @@
+export default {
+    style : {
+        normal : {
+            'background': "#1e130c", 
+            'background' : "-webkit-linear-gradient(to top, #9a8478, #1e130c)", 
+            'background': "linear-gradient(to top, #9a8478, #1e130c)"
+        },
+        poison : {
+            'background': "#ff0099", 
+            'background' : "-webkit-linear-gradient(to top, #ff0099, #493240)", 
+            'background': "linear-gradient(to top, #ff0099, #493240)"
+        },
+        flying:{
+            'background': '#e6dada',  
+            'background': '-webkit-linear-gradient(to bottom, #e6dada, #274046)', 
+            'background': 'linear-gradient(to bottom, #e6dada, #274046)'
+        },
+        ground:{
+            'background': '#ba8b02',  
+            'background': '-webkit-linear-gradient(to top, #ba8b02, #181818)', 
+            'background': 'linear-gradient(to top, #ba8b02, #181818)'
+        },
+        bug:{
+            'background': '#000000',  
+            'background': '-webkit-linear-gradient(to bottom, #000000, #0f9b0f)', 
+            'background': 'linear-gradient(to bottom, #000000, #0f9b0f)'
+        },
+        ghost:{
+            'background': '#ada996',  
+            'background': '-webkit-linear-gradient(to bottom, #ada996, #f2f2f2, #dbdbdb, #eaeaea)', 
+            'background': 'linear-gradient(to bottom, #ada996, #f2f2f2, #dbdbdb, #eaeaea)'
+        },
+        steel:{
+            'background': '##f0f2f0',  
+            'background': '-webkit-linear-gradient(to top, #f0f2f0, #000c40)', 
+            'background': 'linear-gradient(to top, #f0f2f0, #000c40)'
+        },
+        fire: {
+            'background': '#f12711',  
+            'background': '-webkit-linear-gradient(to bottom, #f5af19, #f12711)', 
+            'background': 'linear-gradient(to bottom, #f5af19, #f12711)'
+        },
+        grass:{
+            'background': '#56ab2f',  
+            'background': '-webkit-linear-gradient(to right, #56ab2f, #a8e063)', 
+            'background': 'linear-gradient(to right, #56ab2f, #a8e063)'
+        },
+        electric:{
+            'background': '#fceabb',  
+            'background': '-webkit-linear-gradient(to bottom, #fceabb, #f8b500)', 
+            'background': 'linear-gradient(to bottom, #fceabb, #f8b500)'
+        },
+        water:{
+            'background': '#9cecfb',  
+            'background': '-webkit-linear-gradient(to top, #9cecfb, #65c7f7, #0052d4)', 
+            'background': 'linear-gradient(to top, #9cecfb, #65c7f7, #0052d4)'
+        },
+        psychic:{
+            'background': '#833ab4',  
+            'background': '-webkit-linear-gradient(to bottom, #833ab4, #fd1d1d, #fcb045)', 
+            'background': 'linear-gradient(to bottom, #833ab4, #fd1d1d, #fcb045)'
+        },
+        ice:{
+            'background': '#1c92d2',  
+            'background': '-webkit-linear-gradient(to left, #1c92d2, #f2fcfe)', 
+            'background': 'linear-gradient(to left, #1c92d2, #f2fcfe)'
+        },
+        fairy:{
+            'background': '#a770ef',  
+            'background': '-webkit-linear-gradient(to right, #a770ef, #cf8bf3, #fdb99b)', 
+            'background': 'linear-gradient(to right, #a770ef, #cf8bf3, #fdb99b)'
+        },
+        dragon:{
+            'background': '#333333',  
+            'background': '-webkit-linear-gradient(to bottom, #333333, #dd1818)', 
+            'background': 'linear-gradient(to bottom, #333333, #dd1818)'
+        },
+        dark:{
+            'background': '#000000',  
+            'background': '-webkit-linear-gradient(to bottom, #000000, #434343)',
+            'background': 'linear-gradient(to bottom, #000000, #434343)'
+        },
+        default:{
+            'background': '#1f1c2c',  
+            'background': '-webkit-linear-gradient(to bottom, #1f1c2c, #928dab)',
+            'background': 'linear-gradient(to bottom, #1f1c2c, #928dab)'
+        }
+    }
+} 
