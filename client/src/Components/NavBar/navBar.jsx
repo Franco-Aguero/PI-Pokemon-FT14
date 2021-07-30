476f17819bf8ab0d116fce709114a4d095673d70
unknown <aguerofranco2002@gmail.com>
2021-07-30 10:20:06 -0300

    refactor(client/navBar): repair of button create poke

--- client/src/Components/NavBar/navBar.jsx
+++ client/src/Components/NavBar/navBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Search from './Search/search'
+import IMG from './create.png'
 import Img from './logo.png'
 
 import s from './navBar.module.css'
@@ -10,11 +10,8 @@ const NavBar = () => {
     return (
         <div className={s.body}>
             
-            <Link to="/"><button id={s.btnLogo}><img src={Img} alt="Logo Pokemon" className={s.imgLogo}/></button></Link>
-
-            <Search/>
-
-            <Link to="/home/create"><button id={s.btnCreate}>Create Poke</button></Link>
+            <Link to="/home"><button id={s.btnLogo}><img src={Img} alt="Logo Pokemon" className={s.imgLogo}/></button></Link>
+            <Link to="/home/create"><button id={s.btnCreate}><img src={IMG} alt="Create Pokemon" /></button></Link>
             
         </div>
     )
